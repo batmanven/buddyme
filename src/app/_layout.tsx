c78f330b11e56db950beea5f26e50e49af0165e3
supermanhenry <145008344+priyansh-narang2308@users.noreply.github.com>
2026-04-07 22:25:08 +0530

    feat: The main index fixed with the auth logic

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -13,7 +13,7 @@ if (!publishableKey) {
 export default function RootLayout() {
   return (
     <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
-      <Stack />
+      <Stack screenOptions={{ headerShown: false }} />
     </ClerkProvider>
 
   )
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,15 @@
+import { useAuth } from "@clerk/expo";
+import { Redirect } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
+
+  const { isSignedIn } = useAuth()
+
+  if (!isSignedIn) {
+    return <Redirect href={'/(auth)'} />
+  }
+
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-3xl font-bold text-red-500">NativeWind is working!</Text>
